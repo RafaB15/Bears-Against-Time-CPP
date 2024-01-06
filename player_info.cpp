0b05c4648331da5ad3f76d5a3a1afeb32ed2cd87
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-05 23:56:50 -0300

    First iteration of player info

---
+++ player_info.cpp
@@ -0,0 +1,12 @@
+#include "player_info.hpp"
+#include "constants.hpp"
+
+using namespace Constants;
+
+//Constructor for Player
+PlayerInfo::PlayerInfo(MapElement*** map, char type) : coordinates(map), type(type), lost_time(0), last_move(MOVE_RIGHT), standing_on(nullptr){}
+
+//Returns the coordinates of the player
+Coordinates PlayerInfo::get_coordinates(void) {
+    return this->coordinates;
+}
