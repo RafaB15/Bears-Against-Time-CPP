6f195425ebf9c518120c3edc0a56d3f59e604a2b
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-07 22:53:58 -0300

    adding a map fireworks class

---
+++ map_elements/map_tools/map_fireworks.cpp
@@ -0,0 +1,7 @@
+#include "map_fireworks.hpp"
+#include "../../utils.hpp"
+
+using namespace Constants;
+
+// Constructor for the MapFireworks class
+MapFireworks::MapFireworks(Map map) : MapTool(map, FIREWORKS_REPRESENTATION) {}
---
+++ map_elements/map_tools/map_fireworks.hpp
@@ -0,0 +1,14 @@
+#ifndef __MAP_FIREWORKS_HPP__
+#define __MAP_FIREWORKS_HPP__
+
+#include "../../utils.hpp"
+#include "map_tool.hpp"
+
+using namespace TypeDefinitions;
+
+class MapFireworks : public MapTool {
+    public:
+        MapFireworks(Map map);
+};
+
+#endif // __MAP_FIREWORKS_HPP__
