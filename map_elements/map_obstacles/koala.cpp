a876dd96dfcd2af6186936bd272e94991e41836a
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-09 22:11:22 -0300

    Adding affect game for the koala

--- map_elements/map_obstacles/koala.cpp
+++ map_elements/map_obstacles/koala.cpp
@@ -1,7 +1,13 @@
 #include "koala.hpp"
 #include "../../utils.hpp"
+#include "../../player/player.hpp"
 
 using namespace Constants;
 
 // Constructor for the Koala class
 Koala::Koala(Map map, Coordinates player_coordinates) : MapObstacle(map, KOALA_REPRESENTATION, player_coordinates) {}
+
+// Affect game
+void Koala::affect_game(Game* game) {
+    game->get_player()->encounter_koala(game);
+}
--- map_elements/map_obstacles/koala.hpp
+++ map_elements/map_obstacles/koala.hpp
@@ -9,6 +9,8 @@ using namespace TypeDefinitions;
 class Koala : public MapObstacle {
     public:
         Koala(Map map, Coordinates player_coordinates);
+        //Affect game
+        void affect_game(Game* game);
 };
 
 #endif // __KOALA_HPP__
