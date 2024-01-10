e764d228e36795b7a6df2c4b83a831a23900a003
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-09 22:23:40 -0300

    Adding the affect game for the Battery

--- map_elements/map_tools/map_battery.cpp
+++ map_elements/map_tools/map_battery.cpp
@@ -1,8 +1,14 @@
 #include "map_battery.hpp"
 #include "../../utils.hpp"
+#include "../../player/player.hpp"
 
 using namespace Constants;
 
 //Constructor for valid random MapBattery.
 //Receives a map to check if the coordinate is already occupied.
 MapBattery::MapBattery(Map map, Coordinates player_coordinates) : MapTool(map, BATTERY_REPRESENTATION, player_coordinates) {}
+
+//Affect game
+void MapBattery::affect_game(Game* game) {
+    game->get_player()->recharge_flashlight(MOVEMENTS_BATTERY);
+}
--- map_elements/map_tools/map_battery.hpp
+++ map_elements/map_tools/map_battery.hpp
@@ -8,6 +8,8 @@ using namespace TypeDefinitions;
 class MapBattery : public MapTool {
     public:
         MapBattery(Map map, Coordinates player_coordinates);
+        //Affect game
+        void affect_game(Game* game);
 };
 
 #endif // __MAP_BATTERY_HPP__
