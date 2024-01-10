1740601f9e8c9f897e46c52fc97f3dc58cf22a07
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-01-09 22:11:13 -0300

    Adding affect game for the rock

--- map_elements/map_obstacles/rock.cpp
+++ map_elements/map_obstacles/rock.cpp
@@ -1,7 +1,13 @@
 #include "rock.hpp"
 #include "../../utils.hpp"
+#include "../../player/player.hpp"
 
 using namespace Constants;
 
 // Constructor for the Rock class
 Rock::Rock(Map map, Coordinates player_coordinates) : MapObstacle(map, ROCK_REPRESENTATION, player_coordinates) {}
+
+// Affect game
+void Rock::affect_game(Game* game) {
+    game->get_player()->encounter_rock();
+}
--- map_elements/map_obstacles/rock.hpp
+++ map_elements/map_obstacles/rock.hpp
@@ -9,6 +9,8 @@ using namespace TypeDefinitions;
 class Rock : public MapObstacle {
     public:
         Rock(Map map, Coordinates player_coordinates);
+        //Affect game
+        void affect_game(Game* game);
 };
 
 #endif // __ROCK_HPP__
